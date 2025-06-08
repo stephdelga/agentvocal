8b168e3312b632e99f681c1e3bae4dfedf298ef1
stephdelga <s.delgado33@gmail.com>
2025-06-08 19:55:28 +0200

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import FastAPI, Response
 
 app = FastAPI()
@@ -18,8 +17,3 @@ def incoming_call():
 </Response>'''
     
     return Response(content=twiml, media_type="application/xml")
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.getenv("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
